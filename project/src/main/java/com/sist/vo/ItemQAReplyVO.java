c3d3b9c69acb08395e19d22aca4d2fae73f0a1b9
HiHyerin <116174672+HiHyerin@users.noreply.github.com>
2023-01-16 08:23:06 +0900

    feat: item,category,review,qa,qa_reqly VO 추가 (#8)

---
+++ project/src/main/java/com/sist/vo/ItemQAReplyVO.java
@@ -0,0 +1,39 @@
+package com.sist.vo;
+import java.util.*;
+public class ItemQAReplyVO {
+	private int qarno,qano;
+	private String writer,content;
+	private Date regdate;
+	public int getQarno() {
+		return qarno;
+	}
+	public void setQarno(int qarno) {
+		this.qarno = qarno;
+	}
+	public int getQano() {
+		return qano;
+	}
+	public void setQano(int qano) {
+		this.qano = qano;
+	}
+	public String getWriter() {
+		return writer;
+	}
+	public void setWriter(String writer) {
+		this.writer = writer;
+	}
+	public String getContent() {
+		return content;
+	}
+	public void setContent(String content) {
+		this.content = content;
+	}
+	public Date getRegdate() {
+		return regdate;
+	}
+	public void setRegdate(Date regdate) {
+		this.regdate = regdate;
+	}
+	
+	
+}
---
+++ project/src/main/java/com/sist/vo/ItemVO.java
@@ -0,0 +1,80 @@
+package com.sist.vo;
+import java.util.*;
+public class ItemVO {
+	private int ino,price,sale,stock,like_cnt,jjim_cnt,icno;
+	private String image,name,description,status,brand;
+	public int getIno() {
+		return ino;
+	}
+	public void setIno(int ino) {
+		this.ino = ino;
+	}
+	public int getPrice() {
+		return price;
+	}
+	public void setPrice(int price) {
+		this.price = price;
+	}
+	public int getSale() {
+		return sale;
+	}
+	public void setSale(int sale) {
+		this.sale = sale;
+	}
+	public int getStock() {
+		return stock;
+	}
+	public void setStock(int stock) {
+		this.stock = stock;
+	}
+	public int getLike_cnt() {
+		return like_cnt;
+	}
+	public void setLike_cnt(int like_cnt) {
+		this.like_cnt = like_cnt;
+	}
+	public int getJjim_cnt() {
+		return jjim_cnt;
+	}
+	public void setJjim_cnt(int jjim_cnt) {
+		this.jjim_cnt = jjim_cnt;
+	}
+	public int getIcno() {
+		return icno;
+	}
+	public void setIcno(int icno) {
+		this.icno = icno;
+	}
+	public String getImage() {
+		return image;
+	}
+	public void setImage(String image) {
+		this.image = image;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public String getStatus() {
+		return status;
+	}
+	public void setStatus(String status) {
+		this.status = status;
+	}
+	public String getBrand() {
+		return brand;
+	}
+	public void setBrand(String brand) {
+		this.brand = brand;
+	}
+	
+	
+}
