280e206bad419561cb633207b4a25efe7f0f58bb
daaeson <allaalldo@kakao.com>
2023-01-15 14:19:21 +0900

    OrderVO,DeliveryVO,OrderItemVO 추가

---
+++ project/src/main/java/com/sist/vo/OrderItemVO.java
@@ -0,0 +1,48 @@
+package com.sist.vo;
+public class OrderItemVO {
+	private int oino,price,quantity,ino,ono;
+	private String image,name;
+	
+	public int getOino() {
+		return oino;
+	}
+	public void setOino(int oino) {
+		this.oino = oino;
+	}
+	public int getPrice() {
+		return price;
+	}
+	public void setPrice(int price) {
+		this.price = price;
+	}
+	public int getQuantity() {
+		return quantity;
+	}
+	public void setQuantity(int quantity) {
+		this.quantity = quantity;
+	}
+	public int getIno() {
+		return ino;
+	}
+	public void setIno(int ino) {
+		this.ino = ino;
+	}
+	public int getOno() {
+		return ono;
+	}
+	public void setOno(int ono) {
+		this.ono = ono;
+	}
+	public String getImage() {
+		return image;
+	}
+	public void setImage(String image) {
+		this.image = image;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+}
