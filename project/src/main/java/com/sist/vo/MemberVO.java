02aaf70bc55e971e327fa9e5496abc1deaa63d6c
yujinn001 <14rladbwls@naver.com>
2023-01-19 15:44:15 +0900

    MemberVO 추가

--- project/src/main/java/com/sist/vo/MemberVO.java
+++ project/src/main/java/com/sist/vo/MemberVO.java
@@ -1,5 +1,95 @@
 package com.sist.vo;
-
+// MemberVO
 public class MemberVO {
+	private String mid,password,name,email,tel,birth,postcode,home_addr,detail_addr,sex,role;
 
+	public String getMid() {
+		return mid;
+	}
+
+	public void setMid(String mid) {
+		this.mid = mid;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getTel() {
+		return tel;
+	}
+
+	public void setTel(String tel) {
+		this.tel = tel;
+	}
+
+	public String getBirth() {
+		return birth;
+	}
+
+	public void setBirth(String birth) {
+		this.birth = birth;
+	}
+
+	public String getPostcode() {
+		return postcode;
+	}
+
+	public void setPostcode(String postcode) {
+		this.postcode = postcode;
+	}
+
+	public String getHome_addr() {
+		return home_addr;
+	}
+
+	public void setHome_addr(String home_addr) {
+		this.home_addr = home_addr;
+	}
+
+	public String getDetail_addr() {
+		return detail_addr;
+	}
+
+	public void setDetail_addr(String detail_addr) {
+		this.detail_addr = detail_addr;
+	}
+
+	public String getSex() {
+		return sex;
+	}
+
+	public void setSex(String sex) {
+		this.sex = sex;
+	}
+
+	public String getRole() {
+		return role;
+	}
+
+	public void setRole(String role) {
+		this.role = role;
+	}
+	
+	
 }
