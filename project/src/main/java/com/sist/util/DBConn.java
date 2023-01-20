ec1c0f77f0ec5f03c76ecf1d26d1c9226731fa3e
yoonms0617 <yoonminwater@gmail.com>
2023-01-20 17:29:48 +0900

    feat: DB 연동 기능 추가

---
+++ project/src/main/java/com/sist/util/DBConn.java
@@ -0,0 +1,57 @@
+package com.sist.util;
+
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.sql.DataSource;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public class DBConn {
+
+    private DataSource dataSource;
+
+    private DBConn() {
+        try {
+            Context context = new InitialContext();
+            Context env = (Context) context.lookup("java:comp/env");
+            dataSource = (DataSource) env.lookup("jdbc/oracle");
+        } catch (NamingException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static DBConn getInstance() {
+        return LazyHolder.INSTANCE;
+    }
+
+    public Connection createConnection() {
+        Connection conn = null;
+        try {
+            conn = dataSource.getConnection();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return conn;
+    }
+
+    public void closeConnection(PreparedStatement ps, Connection conn) {
+        try {
+            if (ps != null) {
+                ps.close();
+            }
+            if (conn != null) {
+                conn.close();
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static class LazyHolder {
+        private static final DBConn INSTANCE = new DBConn();
+    }
+
+}
