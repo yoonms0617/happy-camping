5f32ff68b8852f183e286abd7b8c6af89ce2a5ec
yoonms0617 <yoonminwater@gmail.com>
2023-01-22 12:23:07 +0900

    feat: lombok 적용

--- project/src/main/java/com/sist/vo/CampReviewVO.java
+++ project/src/main/java/com/sist/vo/CampReviewVO.java
@@ -1,7 +1,12 @@
 package com.sist.vo;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import java.util.Date;
 
+@Getter
+@Setter
 public class CampReviewVO {
 
     private int clno;
@@ -11,57 +16,5 @@ public class CampReviewVO {
     private String content;
 
     private Date regDate;
-
-    private String cno;
-
-    private String mid;
-
-    public int getClno() {
-        return clno;
-    }
-
-    public void setClno(int clno) {
-        this.clno = clno;
-    }
-
-    public String getWriter() {
-        return writer;
-    }
-
-    public void setWriter(String writer) {
-        this.writer = writer;
-    }
-
-    public String getContent() {
-        return content;
-    }
-
-    public void setContent(String content) {
-        this.content = content;
-    }
-
-    public Date getRegDate() {
-        return regDate;
-    }
-
-    public void setRegDate(Date regDate) {
-        this.regDate = regDate;
-    }
-
-    public String getCno() {
-        return cno;
-    }
-
-    public void setCno(String cno) {
-        this.cno = cno;
-    }
-
-    public String getMid() {
-        return mid;
-    }
-
-    public void setMid(String mid) {
-        this.mid = mid;
-    }
 
 }
