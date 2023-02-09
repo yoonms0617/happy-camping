93fe5def004a3795c9144f228e920c0725ff912a
yoonms0617 <yoonminwater@gmail.com>
2023-02-09 15:23:00 +0900

    feat: 장바구니 기능 추가

--- project/src/main/java/com/sist/dao/CartDAO.java
+++ project/src/main/java/com/sist/dao/CartDAO.java
@@ -1,16 +1,14 @@
 package com.sist.dao;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.util.ArrayList;
-import java.util.List;
+import java.sql.*;
+import java.util.*;
 
 import com.sist.util.DBConn;
-import com.sist.vo.CartVO;
+import com.sist.vo.*;
+
 public class CartDAO {
 
-	private Connection conn;
+    private Connection conn;
     private PreparedStatement ps;
     private final DBConn dbConn;
 
@@ -18,106 +16,116 @@ public class CartDAO {
         this.dbConn = DBConn.getInstance();
     }
 
-	public List<CartVO> goodsCartListData(String mid)
-	{
+    public List<CartVO> goodsCartListData(String mid) {
 
-		List<CartVO> list = new ArrayList<>();
-		try
-		{
-			conn = dbConn.createConnection();
-            String sql = "SELECT cno,i.ino,mid,i.image,i.name,i.price,quantity,i.sale "
-            		+ "FROM hc_cart_2 c, hc_item_2 i "
-            		+ "WHERE c.cno=i.ino "
-            		+ "AND mid=?";
+        List<CartVO> list = new ArrayList<CartVO>();
+        try {
+            conn = dbConn.createConnection();
+            String sql = "SELECT cno,i.ino,mid,c.image,c.name,c.price,quantity,i.sale "
+                    + "FROM hc_cart_2 c, hc_item_2 i "
+                    + "WHERE c.ino = i.ino "
+                    + "AND mid=?";
             ps = conn.prepareStatement(sql);
             ps.setString(1, mid);
-            ResultSet rs=ps.executeQuery();
-            while(rs.next())
-            {
-            	CartVO vo = new CartVO();
-            	vo.setCno(rs.getInt(1));
-            	vo.getIvo().setIno(rs.getInt(2));
-            	vo.setMid(rs.getString(3));
-            	vo.setImage(rs.getString(4));
-            	vo.setName(rs.getString(5));
-            	vo.setPrice(rs.getInt(6));
-            	vo.setQuantity(rs.getInt(7));
-            	vo.getIvo().setSale(rs.getInt(8));
-            	list.add(vo);
+            ResultSet rs = ps.executeQuery();
+            while (rs.next()) {
+                CartVO vo = new CartVO();
+                vo.setCno(rs.getInt(1));
+                vo.getIvo().setIno(rs.getInt(2));
+                vo.setMid(rs.getString(3));
+                vo.setImage(rs.getString(4));
+                vo.setName(rs.getString(5));
+                vo.setPrice(rs.getInt(6));
+                vo.setQuantity(rs.getInt(7));
+                vo.getIvo().setSale(rs.getInt(8));
+                list.add(vo);
             }
             rs.close();
-		}catch(Exception ex)
-		{
-			ex.printStackTrace();
-		}
-		finally
-		{
-			dbConn.closeConnection(ps, conn);
-		}
-		return list;
-	}
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        } finally {
+            dbConn.closeConnection(ps, conn);
+        }
+        return list;
+    }
 
-	public void goodsCartInsert(CartVO vo)
-	{
-		try
-		{
-			conn = dbConn.createConnection();
+    public void goodsCartInsert(CartVO vo) {
+        try {
+            conn = dbConn.createConnection();
             String sql = "INSERT INTO hc_cart_2 VALUES"
-            		+ "(hc_cart_cno_seq_2.nextval,?,?,?,?,?,?)";
-            ps=conn.prepareStatement(sql);
-            ps.setString(1, vo.getImage());
+                    + "(hc_cart_cno_seq_2.nextval,?,?,?,?,?,?)";
+            ps = conn.prepareStatement(sql);
+            String image = vo.getImage();
+            ps.setString(1, image);
             ps.setString(2, vo.getName());
             ps.setInt(3, vo.getPrice());
             ps.setInt(4, vo.getQuantity());
             ps.setInt(5, vo.getIno());
             ps.setString(6, vo.getMid());
             ps.executeUpdate();
-		}catch(Exception ex)
-		{
-			ex.printStackTrace();
-		}
-		finally
-		{
-			dbConn.closeConnection(ps, conn);
-		}
-	}
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        } finally {
+            dbConn.closeConnection(ps, conn);
+        }
+    }
 
-	public void goodsCartSelectDelete(int cno)
-	{
-		try
-		{
-			conn=dbConn.createConnection();
-			String sql = "DELETE FROM hc_cart_2 "
-					+ "WHERE cno=?";
-			ps=conn.prepareStatement(sql);
-			ps.setInt(1, cno);
-			ps.executeUpdate();
-		}catch(Exception ex)
-		{
-			ex.printStackTrace();
-		}
-		finally
-		{
-			dbConn.closeConnection(ps, conn);
-		}
-	}
+    public void goodsCartSelectDelete(int cno) {
+        try {
+            conn = dbConn.createConnection();
+            String sql = "DELETE FROM hc_cart_2 "
+                    + "WHERE cno=?";
+            ps = conn.prepareStatement(sql);
+            ps.setInt(1, cno);
+            ps.executeUpdate();
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        } finally {
+            dbConn.closeConnection(ps, conn);
+        }
+    }
 
-	public void goodsCartAllDelete()
-	{
-		try
-		{
-			conn=dbConn.createConnection();
-			String sql = "DELETE FROM hc_cart_2";
-			ps=conn.prepareStatement(sql);
-			ps.executeUpdate();
-		}catch(Exception ex)
-		{
-			ex.printStackTrace();
-		}
-		finally
-		{
-			dbConn.closeConnection(ps, conn);
-		}
-	}
+    public void goodsCartAllDelete() {
+        try {
+            conn = dbConn.createConnection();
+            String sql = "DELETE FROM hc_cart_2";
+            ps = conn.prepareStatement(sql);
+            ps.executeUpdate();
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        } finally {
+            dbConn.closeConnection(ps, conn);
+        }
+    }
+
+    public void goodsCartUpdateUp(int cno) {
+        try {
+            conn = dbConn.createConnection();
+            String sql = "UPDATE hc_cart_2 SET quantity=quantity+1 "
+                    + "WHERE cno=?";
+            ps = conn.prepareStatement(sql);
+            ps.setInt(1, cno);
+            ps.executeUpdate();
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        } finally {
+            dbConn.closeConnection(ps, conn);
+        }
+    }
+
+    public void goodsCartUpdateDown(int cno) {
+        try {
+            conn = dbConn.createConnection();
+            String sql = "UPDATE hc_cart_2 SET quantity=quantity-1 "
+                    + "WHERE cno=?";
+            ps = conn.prepareStatement(sql);
+            ps.setInt(1, cno);
+            ps.executeUpdate();
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        } finally {
+            dbConn.closeConnection(ps, conn);
+        }
+    }
 
 }
--- project/src/main/java/com/sist/model/CartModel.java
+++ project/src/main/java/com/sist/model/CartModel.java
@@ -1,96 +1,106 @@
 package com.sist.model;
 
-import java.util.List;
-
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
+import java.util.*;
+
 import com.sist.controller.annotation.Controller;
 import com.sist.controller.annotation.RequestMapping;
-import com.sist.dao.CartDAO;
+import com.sist.dao.*;
 import com.sist.vo.CartVO;
 
 @Controller
 public class CartModel {
 
-	private final CartDAO cartDAO;
+    private final CartDAO cartDAO;
 
     public CartModel() {
         this.cartDAO = new CartDAO();
     }
 
-	// 장바구니 페이지 이동 (/cart/cart.do)
+    // 장바구니 페이지 이동 (/cart/cart.do)
     @RequestMapping("cart/cart.do")
-	public String cartList(HttpServletRequest request, HttpServletResponse response)
-	{
-		HttpSession session = request.getSession();
-		String id=(String)session.getAttribute("mid");
-		CartDAO dao = new CartDAO();
-		// DAO 연동
-		List<CartVO> list = dao.goodsCartListData(id);
+    public String cartList(HttpServletRequest request, HttpServletResponse response) {
+        HttpSession session = request.getSession();
+        String id = (String) session.getAttribute("mid");
+        List<CartVO> list = cartDAO.goodsCartListData(id);
 
-		if(list.size()>0)
-		{
-			request.setAttribute("count", list.size());
-			request.setAttribute("list", list);
-		}
-		else
-			request.setAttribute("count", 0);
+        int total_price = 0;
+        if (list.size() > 0) {
+            for (CartVO cart : list) {
+                int price = cart.getPrice();
+                int quantity = cart.getQuantity();
+                total_price += (price * quantity);
+            }
+            request.setAttribute("count", list.size());
+            request.setAttribute("list", list);
+            request.setAttribute("total_price", total_price);
+        } else {
+            request.setAttribute("count", 0);
+        }
 
-		request.setAttribute("main.jsp", "/happy/cart/cart.jsp");
-		return "/happy/cart/cart.jsp";
-	}
+        return "/happy/cart/cart.jsp";
+    }
 
     // 장바구니에 담기
     @RequestMapping("cart/cart_insert.do")
-    public String cartItem(HttpServletRequest request, HttpServletResponse response)
-    {
-    	String iNo = request.getParameter("cIno");
-    	String cAccount = request.getParameter("cAccount");
-    	String cPrice = request.getParameter("cPrice");
-    	String cSale = request.getParameter("cSale");
-    	HttpSession session = request.getSession();
-    	String mid = (String)session.getAttribute("mid");
-    	CartVO vo = new CartVO();
-    	vo.setPrice(Integer.parseInt(cPrice));
-    	vo.setQuantity(Integer.parseInt(cAccount));
-    	vo.setIno(Integer.parseInt(iNo));
-    	vo.setMid(mid);
-    	// DAO 연동
-    	CartDAO dao = new CartDAO();
-    	dao.goodsCartInsert(vo);
-    	return "redirect:cart.do";
+    public String cartItem(HttpServletRequest request, HttpServletResponse response) {
+        String ino = request.getParameter("ino");
+        String image = request.getParameter("image");
+        String name = request.getParameter("name");
+        String quantity = request.getParameter("quantity");
+        String price = request.getParameter("price");
+        HttpSession session = request.getSession();
+        String mid = (String) session.getAttribute("mid");
+        CartVO vo = new CartVO();
+        vo.setImage(image);
+        vo.setName(name);
+        vo.setPrice(Integer.parseInt(price));
+        vo.setQuantity(Integer.parseInt(quantity));
+        vo.setIno(Integer.parseInt(ino));
+        vo.setMid(mid);
+        // DAO 연동
+        cartDAO.goodsCartInsert(vo);
+        return "redirect:cart.do";
     }
 
     // 장바구니 선택 삭제
     @RequestMapping("cart/cart_cancel.do")
-    public String cartSelectDelete(HttpServletRequest request, HttpServletResponse response)
-    {
-    	String cno=request.getParameter("cno");
-    	CartDAO dao = new CartDAO();
-    	dao.goodsCartSelectDelete(Integer.parseInt(cno));
-    	return "redirect:cart.do";
+    public String cartSelectDelete(HttpServletRequest request, HttpServletResponse response) {
+        String cno = request.getParameter("cno");
+        cartDAO.goodsCartSelectDelete(Integer.parseInt(cno));
+        return "redirect:cart.do";
     }
-
-    /*
-    @RequestMapping("goods/cart_cancel.do")
-    public String goods_cart_cancel(HttpServletRequest request, HttpServletResponse response)
-    {
-   		String bno=request.getParameter("bno");
-   		CartDAO dao = new CartDAO();
-   		dao.goodsCartDelete(Integer.parseInt(bno));
-   		return "redirect:cart_list.do";
-    }
-    */
 
     // 장바구니 전체 삭제
     @RequestMapping("cart/cart_cancel_all.do")
-    public String cartAllDelete(HttpServletRequest request, HttpServletResponse response)
-    {
-    	CartDAO dao = new CartDAO();
-    	dao.goodsCartAllDelete();
-    	return "redirect:cart.do";
+    public String cartAllDelete(HttpServletRequest request, HttpServletResponse response) {
+        cartDAO.goodsCartAllDelete();
+        return "redirect:cart.do";
     }
 
+    // 장바구니 수량 증가
+    @RequestMapping("cart/cart_update_up.do")
+    public String cartUpdateUp(HttpServletRequest request, HttpServletResponse response) {
+        String cno = request.getParameter("cno");
+        cartDAO.goodsCartUpdateUp(Integer.parseInt(cno));
+        return "redirect:cart.do";
+    }
+
+    // 장바구니 수량 감소
+    @RequestMapping("cart/cart_update_down.do")
+    public String cartUpdateDown(HttpServletRequest request, HttpServletResponse response) {
+        String cno = request.getParameter("cno");
+        cartDAO.goodsCartUpdateDown(Integer.parseInt(cno));
+        return "redirect:cart.do";
+    }
 }
+
+
+
+
+
+
+
